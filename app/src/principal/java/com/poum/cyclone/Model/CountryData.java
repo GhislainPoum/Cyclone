c71a39846a3863a41826a35647d427a46fbd9c9b
Ghislain Poum <bimbare@gmail.com>
2019-09-29 15:05:48 +0100

    Add files via upload

---
+++ app/src/principal/java/com/poum/cyclone/Model/CountryData.java
@@ -0,0 +1,11 @@
+package com.poum.cyclone.Model;
+
+public class CountryData {
+
+    public static final String[] CountryNames = {
+            "+237"
+    };
+    public static final String[] CountryCodes = {
+            "237"
+    };
+}
